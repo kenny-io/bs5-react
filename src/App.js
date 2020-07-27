27628d83d21546946eb551d14ebf42697cb686a3
kenny-io <ekeneeze3@gmail.com>
2020-07-28 00:23:07 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <h2 class="text-center">Register</h2>
+
+      <div>
+        <div class="mb-1">
+          <label for="email" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div class="mb-1">
+          <label for="password" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        <div class="mb-1">
+          <label for="bio" class="form-label">
+            Tell us about you
+          </label>
+          <textarea class="form-control" id="bio" rows="3"></textarea>
+        </div>
+        <div class="col-12">
+          <button class="btn btn-primary" type="submit">
+            Register
+          </button>
+        </div>
+      </div>
+      <br></br>
+      <div>
+        <h2 class="text-center"> Icons! </h2>
+        <div class="mb-2 input-group">
+          {/* <label for="email" class="form-label">
+            Email address
+          </label> */}
+          <div class="icon">
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              class="bi bi-envelope"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"
+              />
+            </svg>
+          </div>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div>
+          <div>
+            <div class="input-group mb-2 ">
+              <div class="icon">
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  class="bi bi-person "
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                id="inlineFormInputGroupUsername2"
+                placeholder="Username"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="mb-2 input-group">
+          {/* <label for="password" class="form-label">
+            Password
+          </label>
+           */}
+          <div class="icon">
+            <svg
+              width="1em"
+              height="em"
+              viewBox="0 0 16 16"
+              class="bi bi-lock"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"
+              />
+            </svg>
+          </div>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        {/* <div class="mb-3">
+          <label for="cpassword" class="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Confirm Password"
+          />
+        </div> */}
+        <div class="col-12">
+          <button class="btn btn-primary" type="submit">
+            Register
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
